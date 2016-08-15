9a54ed7394980b012ad23252bbe73f45b1d1930b
Yuuki Kawasaki <yuku08@gmail.com>
2016-08-15 06:53:46 +0000

    lesson6 challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+  # 年齢は任意入力かつ0以上の数字
+  validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 150, allow_blank: true }
 end
